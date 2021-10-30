c1d3d6a480b4d7b1b630fe905a02881f10f29243
Kingsley Victor <javaprodigy56@gmail.com>
2021-10-30 22:03:47 +0100

    prettify code

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -5,29 +5,62 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Airdrop is Context, Ownable {
-  uint256 _dropAmountPerAddress;
-  address _token;
+  struct Drop {
+    address _recipient;
+    uint256 _amount;
+  }
 
-  constructor(uint256 dropAmountPerAddress_, address token_) Ownable() {
-    _dropAmountPerAddress = dropAmountPerAddress_;
+  address _token;
+  address[] _all;
+
+  mapping(address => uint256) _amounts;
+
+  constructor(address token_) Ownable() {
     _token = token_;
   }
 
-  function execAirdrop(address[] memory _addresses) external onlyOwner() returns (bool) {
-    require(IERC20(_token).balanceOf(address(this)) >= (_dropAmountPerAddress * _addresses.length), "Not enough tokens");
+  function addRecipients(Drop[] memory _drops) external onlyOwner {
+    uint256 _total = 0;
 
-    for (uint i = 0; i < _addresses.length; i++) {
-      require(IERC20(_token).transfer(_addresses[i], _dropAmountPerAddress), "Could not transfer tokens");
+    for (uint256 i = 0; i < _drops.length; i++)
+      _total = _total + _drops[i]._amount;
+
+    require(
+      IERC20(_token).balanceOf(address(this)) >= _total,
+      "Not enough tokens"
+    );
+
+    for (uint256 i = 0; i < _drops.length; i++) {
+      bool _isPresent = false;
+      for (uint256 j = 0; j < _all.length; j++) {
+        if (_all[j] == _drops[i]._recipient) {
+          _isPresent = true;
+        }
+      }
+      if (!_isPresent) _all.push(_drops[i]._recipient);
     }
 
-    return true;
+    for (uint256 i = 0; i < _drops.length; i++)
+      _amounts[_drops[i]._recipient] = _drops[i]._amount;
   }
 
-  function setToken(address token_) external onlyOwner() {
+  function executeAirdrop() external onlyOwner {
+    for (uint256 i = 0; i < _all.length; i++) {
+      require(
+        IERC20(_token).transfer(_all[i], _amounts[_all[i]]),
+        "Error occured while transfering tokens"
+      );
+      _amounts[_all[i]] = 0;
+    }
+    uint256 _length = _all.length;
+
+    while (_length > 0) {
+      _all.pop();
+      _length = _length - 1;
+    }
+  }
+
+  function setToken(address token_) external onlyOwner {
     _token = token_;
-  }
-
-  function setDropAmountPerAddress(uint256 dropAmountPerAddress_) external onlyOwner() {
-    _dropAmountPerAddress = dropAmountPerAddress_;
   }
 }
